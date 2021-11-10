ceff5827bbb3af3a915db6c25439c89cb6e98bbb
Paolo Caleffi <p.caleffi@dreamonkey.com>
2021-11-10 17:38:13 +0100

    feat: use table instead of list

--- src/pages/locations.vue
+++ src/pages/locations.vue
@@ -1,33 +1,69 @@
 <template>
   <q-page>
-    <q-list separator>
-      <location-item
-        v-for="location in locations"
-        :key="location.name"
-        :location="location"
-      />
-    </q-list>
+    <q-table :columns="columns" :rows="rows" />
   </q-page>
 </template>
 
 <script lang="ts">
-import LocationItem from 'src/components/location-item.vue';
-import { LocationEndpoint } from 'src/models/location';
+import { LocationEndpoint, LocationStats } from 'src/models/location';
 import { defineComponent, onMounted, ref } from 'vue';
+
+const columns = [
+  {
+    name: 'name',
+    label: 'Location',
+    field: 'name',
+    align: 'left',
+    classes: 'col-8',
+  },
+  {
+    name: 'temperature',
+    label: 'Temperature',
+    field: 'temperature',
+    format: (val?: string) => `${val ?? 'n/a'} °C`,
+    classes: 'col-2',
+  },
+  {
+    name: 'humidity',
+    label: 'Humidity',
+    field: 'humidity',
+    format: (val?: string) => `${val ?? 'n/a'} %`,
+    classes: 'col-2',
+  },
+];
+
+interface LocationRow {
+  name: string;
+  temperature?: string;
+  humidity?: string;
+}
 
 export default defineComponent({
   name: 'PageLocations',
-  components: { LocationItem },
   setup() {
-    const locations = ref<LocationEndpoint[]>([]);
+    const rows = ref<LocationRow[]>([]);
 
     onMounted(async () => {
-      locations.value = (await (
+      const locations = (await (
         await fetch('location-list.json')
       ).json()) as LocationEndpoint[];
+
+      for (let index = 0; index < locations.length; index++) {
+        const { name, url } = locations[index];
+        const row: LocationRow = { name };
+        rows.value.push(row);
+
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        fetch(url)
+          .then((response) => response.json())
+          .then(
+            ({ temp: temperature, humid: humidity }: LocationStats) =>
+              (rows.value[index] = { name, temperature, humidity })
+          );
+      }
     });
 
-    return { locations };
+    return { columns, rows };
   },
 });
 </script>
